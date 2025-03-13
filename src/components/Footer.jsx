c69fc086ea608bcc0a1a039054111421c29f5fc1
EzequielMoreira <eezequielmoreira@gmail.com>
2025-03-13 18:33:33 -0300

    creacion de archivo footer jsx y su contenido colocado

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './BarraNav.css';
+
+function Footer(){
+
+    return(
+        <div className="footer">
+            <h3>Pie de pagina de practica, proximo a modificar</h3>
+        </div>
+    )
+}
+
+export default Footer;
