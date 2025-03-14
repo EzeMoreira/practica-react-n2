57fc8afe427d3ee16d79ffb3460d181a51531ce0
EzequielMoreira <eezequielmoreira@gmail.com>
2025-03-14 18:18:35 -0300

    creacion de productos.jsx mas su contenido

---
+++ src/components/Productos.jsx
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from "react";
+import { getAllProductos } from "../servicios/ProductoServicio";
+import Producto from "./Producto";
+
+function Producto() {
+
+    const [loading, setLoading] = useState(true)
+    const [productos, setProductos] = useState();
+    const [buscar, setBuscar] = useState("iphone");
+
+    useEffect(() => {
+        const request = async () => {
+
+            try {
+                const response = await getAllProductos(buscar);
+
+                console.log("Desde los productos", response.results);
+
+                setProductos(response.results)
+                setLoading(false);
+            } catch (evento) {
+                console.log(evento);
+            }
+        };
+
+        request();
+    }, [buscar]);
+
+    const handleChange = (event) => {
+
+        const value = event.target.value;
+        console.log(value);
+        setBuscar(value);
+    }
+
+    if (loading) {
+        return(
+            <div>Cargando...</div>
+        )
+    }   else {
+        return(
+            <>
+            <div id="buscador">
+                <input type="text"
+                name="buscar"
+                value={buscar}
+                onChange={handleChange}
+                placeholder="buscar"
+                /> <br />
+            </div>
+
+            {productos.slice(0, 8).map((producto) =>
+            <Producto 
+            id={producto.id}
+            imagen={producto.thumbnail}
+            nommbre={producto.title}
+            precio={producto.price}
+            />
+            )}
+            <br />
+            </>
+        );
+    }
+}
+
+export default Productos;
