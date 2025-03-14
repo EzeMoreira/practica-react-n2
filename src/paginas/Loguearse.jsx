8a53cdaf8ac0243c8ca3ced6e82b00df5de7c092
EzequielMoreira <eezequielmoreira@gmail.com>
2025-03-14 18:34:57 -0300

    creacion de componente loguarse jsx mas su contenido

---
+++ src/paginas/Loguearse.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Loguearse() {
+
+    return(
+        <div className="registro">
+            <h2>Accede</h2>
+            <form action="#" method="GET">
+                <label>
+                    Email:
+                </label> <br />
+                <input type="email" name="email" required placeholder="email"/> <br />
+                <label>
+                    Password:
+                </label> <br />
+                <input type="password" name="password" placeholder="password" required/>
+                <br />
+                <button type="submit" id="botonComprar">Enviar</button> <br />
+            </form>
+        </div>
+    );
+}
+
+export default Loguearse;
