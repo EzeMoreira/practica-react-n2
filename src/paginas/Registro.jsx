aa78b3b4e01541edc37ddfda1676b776fad5b5bb
EzequielMoreira <eezequielmoreira@gmail.com>
2025-03-14 18:42:21 -0300

    creacion de componente registro mas su contenido

---
+++ src/paginas/Registro.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+function Registro() {
+
+    return(
+        <>
+        <h2>Registrate para +</h2>
+        <div className="registro">
+            <form action="#" method="GET">
+                <label>
+                    Nombre:
+                </label> <br />
+                <input type="text" name="name" placeholder="Nombre" required /> <br/>
+                <label>
+                    Email:
+                </label> <br />
+                <input type="email" name="email" placeholder="email" required /> <br/>
+                <label>
+                    Password:
+                </label> <br />
+                <input type="password" name="password" placeholder="password" required />
+                <br/>
+                <button type="submit" value="submit" id="botonComprar">Enviar</button> <br />
+            </form>
+        </div>
+        </>
+    );
+}
+
+export default Registro;
