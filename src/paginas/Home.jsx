b60e646459195ff570c3808e8779b78eb351872f
EzequielMoreira <eezequielmoreira@gmail.com>
2025-03-14 18:29:07 -0300

    creacion de contenido dentro de home jsx

---
+++ src/paginas/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Productos from "../components/Productos";
+
+function Home(){
+
+    return(
+        <div>
+            <h2>Productos seleccionados</h2>
+            <Productos />
+        </div>
+    );
+}
+
+export default Home;
