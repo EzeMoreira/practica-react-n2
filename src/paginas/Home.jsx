a9a2c39a363b27964ac63bc5a53a94cacb2cecf1
EzequielMoreira <eezequielmoreira@gmail.com>
2025-03-15 11:53:06 -0300

    agrgado de imagen en home.jsx

--- src/paginas/Home.jsx
+++ src/paginas/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Productos from "../components/Productos";
+import logo from '../img/elije-tu-lenguaje.png'
 
 function Home(){
 
     return(
         <div>
             <h2>Productos seleccionados</h2>
+            <img src={logo} className='AppLogo' alt='banner' />
             <Productos />
         </div>
     );
