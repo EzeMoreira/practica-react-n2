2441a2da74b900e4d4758dcdfe8d5b006451c112
EzequielMoreira <eezequielmoreira@gmail.com>
2025-03-21 15:06:17 -0300

    archivo firebase renombrado y modificado en los imports correspondientes

--- src/servicios/usuariosService.jsx
+++ src/servicios/usuariosService.jsx
@@ -1,4 +1,4 @@
-import firebase from "../Config/firebase";
+import firebase from "../Config/Firebase";
 
 export async function create(payload){
     const responseUser = await firebase.auth().createUserWithEmailAndPassword(payload.email)
