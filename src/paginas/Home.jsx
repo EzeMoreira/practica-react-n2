b081d3ebef3aa22b5440929d1e69f0266126883c
EzequielMoreira <eezequielmoreira@gmail.com>
2025-03-21 12:19:21 -0300

    home jsx modificado, importado firebase para realizar pruebas

--- src/paginas/Home.jsx
+++ src/paginas/Home.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Productos from "../components/Productos";
-import logo from '../img/elije-tu-lenguaje.png'
+import logo from '../img/elije-tu-lenguaje.png';
+import firebase from '../Config/Firebase';
 
 function Home(){
-
+    console.log("Esto es firebase: ", firebase);
     return(
         <div>
             <h2>Las opciones</h2>
