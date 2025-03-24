9af93ec759400d1660a0b2ee67c39a3c4344c1d6
EzequielMoreira <eezequielmoreira@gmail.com>
2025-03-24 13:18:45 -0300

    quitamos form viejo en archivo login jsx

--- src/paginas/Loguearse.jsx
+++ src/paginas/Loguearse.jsx
@@ -18,18 +18,7 @@ function Loguearse() {
     return(
         <div className="registro">
             <h2>Accede</h2>
-            <form action="#" method="GET">
-                <label>
-                    Email:
-                </label> <br />
-                <input type="email" name="email" required placeholder="Email"/> <br />
-                <label>
-                    Password:
-                </label> <br />
-                <input type="password" name="password" placeholder="Password" required/>
-                <br /> <br />
-                <button type="submit" id="botonComprar">Enviar</button> <br />
-            </form>
+            
         </div>
     );
 }
