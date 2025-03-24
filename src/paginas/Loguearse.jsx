6dbc9cb87d4cf3a7b01a19115864595830d1e08a
EzequielMoreira <eezequielmoreira@gmail.com>
2025-03-24 13:15:40 -0300

    imports modificados en archivo loguearse jsx

--- src/paginas/Loguearse.jsx
+++ src/paginas/Loguearse.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+import { login } from '../servicios/usuariosService';
 
 function Loguearse() {
 
